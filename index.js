b5f55dcca7fd8d5cd8a77485ab63967c90d52b9e
Yomaira Escano <yjescano@gmail.com>
2019-02-04 01:24:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // Code your solutions in this file
 function printBadges(names) {
   for (let i = 0; i < names.length; i++) {
-    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`)
+    console.log(`Welcome ${names[i]}! You are employee #${i + 1}.`);
   }
-  return names
+  return names;
 }
 
 function tailsNeverFails() {
-  let tails = 0
+  let tails = 0;
+  
   while (Math.random() >= 0.5) {
-    tails++
+    tails++;
   }
-  return (`You got ${tails} tails in a row!`)
+  return `You got ${tails} tails in a row!`;
 }
